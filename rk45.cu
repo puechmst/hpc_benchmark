a8ba7d35cce82270623bedf8ebbe9a4163f7d4ab
stephane <stephane.puechmorel@enac.fr>
2025-02-26 15:34:12 +0100

    Templated solver.

--- rk45.cu
+++ rk45.cu
@@ -3,59 +3,71 @@
 #include <math.h>
 #include <cuda.h>
 #include <iostream>
+#include <fstream>
 #include <random>
 
-const float A11 = 1.0 / 4.0;
-const float A21 = 3.0 / 32.0;
-const float A22 = 9.0 / 32.0;
-const float A31 = 1932.0 / 2197.0;
-const float A32 = -7200.0 / 2197.0;
-const float A33 = 7296.0 / 2197.0;
-const float A41 = 439.0 / 216.0;
-const float A42 = -8.0;
-const float A43 = 3680.0 / 513.0;
-const float A44 = -845.0 / 4104.0;
-const float A51 = -8.0 / 27.0;
-const float A52 = 2.0;
-const float A53 = -3544.0 / 2565.0;
-const float A54 = 1859.0 / 4104.0;
-const float A55 = -11.0 / 40.0;
+__device__ const float A11 = 1.0f / 4.0f;
+__device__ const float A21 = 3.0f / 32.0f;
+__device__ const float A22 = 9.0f / 32.0f;
+__device__ const float A31 = 1932.0f / 2197.0f;
+__device__ const float A32 = -7200.0f / 2197.0f;
+__device__ const float A33 = 7296.0f / 2197.0f;
+__device__ const float A41 = 439.0f / 216.0f;
+__device__ const float A42 = -8.0f;
+__device__ const float A43 = 3680.0f / 513.0f;
+__device__ const float A44 = -845.0f / 4104.0f;
+__device__ const float A51 = -8.0f / 27.0f;
+__device__ const float A52 = 2.0f;
+__device__ const float A53 = -3544.0f / 2565.0f;
+__device__ const float A54 = 1859.0f / 4104.0f;
+__device__ const float A55 = -11.0f / 40.0f;
 
-const float B11 = 25.0 / 216.0;
-const float B12 = 0.0;
-const float B13 = 1408.0 / 2565.0;
-const float B14 = 2197.0 / 4101.0;
-const float B15 = -1.0 / 5.0;
+__device__ const float B11 = 25.0f / 216.0f;
+__device__ const float B12 = 0.0f;
+__device__ const float B13 = 1408.0f / 2565.0f;
+__device__ const float B14 = 2197.0f / 4101.0f;
+__device__ const float B15 = -1.0f / 5.0f;
 
-const float B21 = 16.0 / 135.0;
-const float B22 = 0.0;
-const float B23 = 6656.0 / 12825.0;
-const float B24 = 28561.0 / 56430.0;
-const float B25 = -9.0 / 50.0;
-const float B26 = 2.0 / 55.0;
+__device__ const float B21 = 16.0f / 135.0f;
+__device__ const float B22 = 0.0f;
+__device__ const float B23 = 6656.0f / 12825.0f;
+__device__ const float B24 = 28561.0f / 56430.0f;
+__device__ const float B25 = -9.0f / 50.0f;
+__device__ const float B26 = 2.0f / 55.0f;
 
-const float C2 = 1.0 / 4.0;
-const float C3 = 3.0 / 8.0;
-const float C4 = 12.0 / 13.0;
-const float C5 = 1.0;
-const float C6 = 1.0 / 2.0;
+__device__ const float C2 = 1.0f / 4.0f;
+__device__ const float C3 = 3.0f / 8.0f;
+__device__ const float C4 = 12.0f / 13.0f;
+__device__ const float C5 = 1.0f;
+__device__ const float C6 = 1.0f / 2.0f;
 
 // the dimension of the state space must be small enough to fit into local registers (255).
 // static definition allows the compiler to unroll loops
+// test
 
 #define STATE_DIM (10)
 
-#define BSIZE (100)
-#define NEQ (100 * BSIZE)
+#define BSIZE (200)
+#define NEQ (1000 * BSIZE)
 
-__device__ void sysdyn(float t, float *y, float *yp)
-{
-    // solution: y = tan(t)
-    for (int i = 0; i < STATE_DIM; i++)
-        yp[i] = 1.0f + y[i] * y[i];
-}
 
-__global__ void rk45(float t, float *y, float *err, float step)
+// __device__ void sysdyn(float t, float *y, float *yp)
+// {
+//     // solution: y = tan(t)
+//     for (int i = 0; i < STATE_DIM; i++)
+//         yp[i] = 1.0f + y[i] * y[i];
+// }
+
+struct my_test {
+    __device__ void operator()(float t, float *y, float *yp) {
+        for (int i = 0; i < STATE_DIM; i++)
+            yp[i] = 1.0f + y[i] * y[i];
+    }
+};
+
+template<class T>
+__global__ 
+void rk45(T ode, float t, float *y, float *err, float step)
 {
     // arrays are normally stored in registers unless STATE_DIM is too large
     // the -Xptvas -v option in CmakeLists.txt dumps true usage.
@@ -65,22 +77,22 @@ __global__ void rk45(float t, float *y, float *err, float step)
     // load local data
     for (int i = 0; i < STATE_DIM; i++)
         yy[i] = y[idx + i];
-    sysdyn(t, yy, k1);
+    ode(t, yy, k1);
     for (int i = 0; i < STATE_DIM; i++)
         cur[i] = yy[i] + step * A11 * k1[i];
-    sysdyn(t + step * C2, cur, k2);
+    ode(t + step * C2, cur, k2);
     for (int i = 0; i < STATE_DIM; i++)
         cur[i] = yy[i] + step * (A21 * k1[i] + A22 * k2[i]);
-    sysdyn(t + step * C3, cur, k3);
+    ode(t + step * C3, cur, k3);
     for (int i = 0; i < STATE_DIM; i++)
         cur[i] = yy[i] + step * (A31 * k1[i] + A32 * k2[i] + A33 * k3[i]);
-    sysdyn(t + step * C4, cur, k4);
+    ode(t + step * C4, cur, k4);
     for (int i = 0; i < STATE_DIM; i++)
         cur[i] = yy[i] + step * (A41 * k1[i] + A42 * k2[i] + A43 * k3[i] + A44 * k4[i]);
-    sysdyn(t + step * C5, cur, k5);
+    ode(t + step * C5, cur, k5);
     for (int i = 0; i < STATE_DIM; i++)
         cur[i] = yy[i] + step * (A51 * k1[i] + A52 * k2[i] + A53 * k3[i] + A54 * k4[i] + A55 * k5[i]);
-    sysdyn(t + step * C6, cur, k6);
+    ode(t + step * C6, cur, k6);
     // get new state and estimate error
     e = 0.0;
     for (int i = 0; i < STATE_DIM; i++)
@@ -91,6 +103,23 @@ __global__ void rk45(float t, float *y, float *err, float step)
         e += step * fabs((B11 - B21) * k1[i] + (B12 - B22) * k2[i] + (B13 - B23) * k3[i] + (B14 - B24) * k4[i] + (B15 - B25) * k5[i] - B26 * k6[i]);
     }
     err[blockIdx.x * blockDim.x + threadIdx.x] = e;
+}
+
+void dump_properties(std::ofstream &of) {
+    // enumerare devices
+    int ndevices;
+    cudaDeviceProp prop;
+    cudaGetDeviceCount(&ndevices);
+    for(int i = 0 ; i < ndevices ; i++) {
+        of << "Device " << i <<  ":" << std::endl;
+        cudaGetDeviceProperties(&prop, i);
+        of << "name : " << prop.name << std::endl;
+        of << "arch : " << prop.major << "." << prop.minor << std::endl;
+        of << "global memory : " << prop.totalGlobalMem << std::endl;
+        of << "shared memory (per block) : " << prop.sharedMemPerBlock << std::endl;
+        of << "registers (per block) : " << prop.regsPerBlock << std::endl;
+        of << "registers (per mp) : " << prop.regsPerMultiprocessor << std::endl;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -104,6 +133,16 @@ int main(int argc, char *argv[])
     y = new float[NEQ * STATE_DIM];
     ys = new float[NEQ * STATE_DIM];
     err = new float[NEQ];
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    // file for saving results
+    std::ofstream res_file("res.txt");
+    // dump capabilities
+    dump_properties(res_file);
+    res_file << "neq : " << NEQ << std::endl;
+    res_file << "dim : " << STATE_DIM << std::endl;
+    res_file << "nb : " << nb << std::endl;
     // populate state randomly and init error
     for (int i = 0; i < NEQ; i++)
     {
@@ -115,12 +154,20 @@ int main(int argc, char *argv[])
     // copy to device
     cudaMemcpy(dy, y, NEQ * STATE_DIM * sizeof(float), cudaMemcpyHostToDevice);
     // linear grid
-    rk45<<<nb, BSIZE>>>(0.0, dy, derr, step);
-    cudaDeviceSynchronize();
+    cudaEventRecord(start);
+    rk45<<<nb, BSIZE>>>(my_test(), 0.0, dy, derr, step);
+    cudaEventRecord(stop);
+    //cudaDeviceSynchronize();
     cudaMemcpy(ys, dy, sizeof(float) * NEQ * STATE_DIM, cudaMemcpyDeviceToHost);
     cudaMemcpy(err, derr, sizeof(float) * NEQ, cudaMemcpyDeviceToHost);
+    cudaEventSynchronize(stop);
     cudaFree(derr);
     cudaFree(dy);
+    float millis=0;
+    cudaEventElapsedTime(&millis, start, stop);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    res_file << "Elapsed : " << millis << std::endl;
     float yt;
     float te;
     float max_err = 0.0;
@@ -142,4 +189,6 @@ int main(int argc, char *argv[])
     std::cout << "max error: " << max_err << " max predicted error: " << max_pred_err << std::endl;
     delete[] y;
     delete[] err;
+    res_file.close();
+    return 0;
 }
